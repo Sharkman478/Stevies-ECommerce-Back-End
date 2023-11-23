29b4319f488ffc61bd59b9b2754d0e353e1783cd
Sharkman478 <steviehelber@gmail.com>
2023-11-22 21:35:15 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Stevies-ECommerce-Back-End
+
+Description
+* This project we were tasked with creating a code that functions as a back-end for an Ecommerce. We needed to build the routes and the models for the project. Also needed to change other things in-order for it to work.
+
+Walkthrough Video
+* 
--- models/Product.js
+++ models/Product.js
@@ -23,7 +23,7 @@ Product.init(
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        DECIMAL,
+        isDecimal: true,
       },
     },
     stock: {
@@ -31,13 +31,13 @@ Product.init(
       allowNull: false,
       defaultValue: [10],
       validate: {
-        isNumeric,
+        isNumeric: true,
       },
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: Category,
+        model: 'category',
         key: 'id',
       },
     }
